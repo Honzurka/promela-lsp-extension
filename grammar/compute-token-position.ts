0ce2ad43bbe905ee0cf8d32913f15b0965b6a08a
Honzurka <jfuerst98@gmail.com>
2024-01-28 19:09:44 +0100

    Working simple autocompletion

---
+++ grammar/compute-token-position.ts
@@ -0,0 +1,68 @@
+// heavily inspired by https://github.com/Strumenta/article-antlr4-c3/
+
+import {ParseTree, TerminalNode, ParserRuleContext, Token, TokenStream} from "antlr4ng";
+
+export type CaretPosition = { line: number, column: number };
+export type TokenPosition = { index: number, context: ParseTree, text: string };
+
+export function tokenPositionComputer(identifierTokenTypes: number[] = []) {
+    return (parseTree: ParseTree, tokens: TokenStream, caretPosition: CaretPosition) =>
+        computeTokenPosition(parseTree, tokens, caretPosition, identifierTokenTypes);
+}
+
+export function computeTokenPosition(
+    parseTree: ParseTree, tokens: TokenStream, caretPosition: CaretPosition, identifierTokenTypes: number[] = []): TokenPosition {
+    if(parseTree instanceof TerminalNode) {
+        return computeTokenPositionOfTerminal(parseTree, tokens, caretPosition, identifierTokenTypes)!;
+    } else {
+        return computeTokenPositionOfChildNode(parseTree as ParserRuleContext, tokens, caretPosition, identifierTokenTypes)!;
+    }
+}
+
+function positionOfToken(token: Token, text: string, caretPosition: CaretPosition, identifierTokenTypes: number[], parseTree: ParseTree) {
+    const start = token.column; // was charPositionInLine;
+    const stop = token.column + text.length;
+    if (token.line == caretPosition.line && start <= caretPosition.column && stop >= caretPosition.column) {
+        let index = token.tokenIndex;
+        if (identifierTokenTypes.includes(token.type)) {
+            index--;
+        }
+        return {
+            index: index,
+            context: parseTree,
+            text: text.substring(0, caretPosition.column - start)
+        };
+    } else {
+        return undefined;
+    }
+}
+
+function computeTokenPositionOfTerminal(
+    parseTree: TerminalNode, tokens: TokenStream, caretPosition: CaretPosition, identifierTokenTypes: number[]) {
+    const token = parseTree.symbol;
+    const text = parseTree.getText();
+    return positionOfToken(token, text, caretPosition, identifierTokenTypes, parseTree);
+}
+
+function computeTokenPositionOfChildNode(
+    parseTree: ParserRuleContext, tokens: TokenStream, caretPosition: CaretPosition, identifierTokenTypes: number[]) {
+    if((parseTree.start && parseTree.start.line > caretPosition.line) ||
+        (parseTree.stop && parseTree.stop.line < caretPosition.line)) {
+        return undefined;
+    }
+    for (let i = 0; i < parseTree.children!.length; i++) {
+        const position = computeTokenPosition(parseTree.getChild(i)!, tokens, caretPosition, identifierTokenTypes);
+        if (position !== undefined) {
+            return position;
+        }
+    }
+    if(parseTree.start && parseTree.stop) {
+        for(let i = parseTree.start.tokenIndex; i <= parseTree.stop.tokenIndex; i++) {
+            const pos = positionOfToken(tokens.get(i), tokens.get(i).text!, caretPosition, identifierTokenTypes, parseTree);
+            if(pos) {
+               return pos;
+            }
+        }
+    }
+    return undefined;
+}
--- grammar/main.ts
+++ grammar/main.ts
@@ -1,17 +1,48 @@
-import { CommonTokenStream, BaseErrorListener, CharStreams } from 'antlr4ng';
-import { PromelaLexer } from './generated/PromelaLexer';
-import { PromelaParser } from './generated/PromelaParser';
+import { CommonTokenStream, BaseErrorListener, CharStreams, ParseTree, TerminalNode } from 'antlr4ng';
+import { CodeCompletionCore } from 'antlr4-c3';
+import { PromelaLexer } from './generated/PromelaLexer.js';
+import { PromelaParser } from './generated/PromelaParser.js';
+import { CaretPosition, computeTokenPosition } from './compute-token-position.js';
 
-const input = CharStreams.fromString("var c = a + b()");
-const lexer = new PromelaLexer(input);
-const parser = new PromelaParser(new CommonTokenStream(lexer));
+export function getSuggestion(code: string, caretPosition: CaretPosition) {
+    const input = CharStreams.fromString(code);
+    const lexer = new PromelaLexer(input);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new PromelaParser(tokenStream);
+    const tree = parser.spec();
 
-const tree = parser.spec();
-console.log(tree);
+    const position = computeTokenPosition(tree, tokenStream, caretPosition);
+    if (position === undefined) {
+        return [];
+    }
+    
+    const core = new CodeCompletionCore(parser);
+    const candidates = core.collectCandidates(position.index);
+    
+    const keywords: string[] = [];
+    const other: string[] = []; //todo
+    for (const candidate of candidates.tokens) {
+        const symbolicName = parser.vocabulary.getSymbolicName(candidate[0]);
+        if (candidate[0] == PromelaParser.NAME) {
+            // skip - handled elsewhere
+        }
+        else if (symbolicName) {
+            other.push(symbolicName.toLowerCase());
+        }
+        else { // keywords
+            const keyword = parser.vocabulary.getDisplayName(candidate[0]);
+            if (keyword) {
+                keywords.push(keyword.toLocaleLowerCase().substring(1, keyword.length - 1));
+            }
+            else {
+                console.error("Unknown token type: " + candidate[0]);
+            }
+        }
+    }
 
-// const errorListener = new ErrorListener();
-// parser.addErrorListener(errorListener);
-// const tree = parser.expression();
+    return [keywords, other];
+}
 
-// const core = new c3.CodeCompletionCore(parser);
-// const candidates = core.collectCandidates(0);
+const result = getSuggestion("bool flags[2];", { line: 1, column: 0 });
+console.log(result);
+
